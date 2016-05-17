5f879da1e5591c1a514f8f3c47405fe238bd2c47
cjling <cjling@aliyun.com>
2016-05-18 01:32:25 +0800

    fix easypr

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -4,14 +4,22 @@ include $(CLEAR_VARS)
 
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
-OPENCV_LIB_TYPE:=STATIC
-# OPENCV_LIB_TYPE:=SHARED
+# OPENCV_LIB_TYPE:=STATIC
+OPENCV_LIB_TYPE:=SHARED
 
 include /Users/cjling/proj/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 
 LOCAL_MODULE := recognize
-LOCAL_SRC_FILES := recognize.cpp
+
+FILE_LIST := $(wildcard $(LOCAL_PATH)/core/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/preprocess/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/util/*.cpp)
+FILE_LIST += recognize.cpp
+
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+# LOCAL_SRC_FILES := recognize.cpp
 LOCAL_LDLIBS += -lm -llog
 
 include $(BUILD_SHARED_LIBRARY)
--- app/src/main/jni/recognize.cpp
+++ app/src/main/jni/recognize.cpp
@@ -4,27 +4,11 @@
 
 #include <jni.h>
 #include <com_link_trust_platerecognize_MainActivity.h>
-#include <opencv2/opencv.hpp>
-using namespace cv;
-
-IplImage * change4channelTo3InIplImage(IplImage * src) {
-  if (src->nChannels != 4) {
-    return NULL;
-  }
-
-  IplImage * destImg = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);
-  for (int row = 0; row < src->height; row++) {
-    for (int col = 0; col < src->width; col++) {
-      CvScalar s = cvGet2D(src, row, col);
-      cvSet2D(destImg, row, col, s);
-    }
-  }
-
-  return destImg;
-}
+#include "include/easypr.h"
 
 JNIEXPORT jstring JNICALL Java_com_link_1trust_platerecognize_MainActivity_recognize
         (JNIEnv * env, jobject obj) {
+    auto results = easypr::api::plate_recognize(NULL, NULL, NULL);
     return env->NewStringUTF("successfully!");
 }
 

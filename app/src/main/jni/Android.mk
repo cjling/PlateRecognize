399f1b5b00360dfca9e5de69ed20dd8b3c0ef440
cjling <cjling@aliyun.com>
2016-05-20 19:30:33 +0800

    更正Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -12,14 +12,23 @@ include /Users/cjling/proj/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE := recognize
 
-FILE_LIST := $(wildcard $(LOCAL_PATH)/core/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/preprocess/*.cpp)
-FILE_LIST += $(wildcard $(LOCAL_PATH)/util/*.cpp)
-FILE_LIST += recognize.cpp
+FILE_LIST := $(wildcard $(LOCAL_PATH)/src/core/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/src/preprocess/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/src/util/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/src/train/*.cpp)
+
 
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_SRC_FILES += recognize.cpp
 # LOCAL_SRC_FILES := recognize.cpp
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/easypr
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/easypr/core
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/easypr/preprocess
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/easypr/train
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/inculde/easypr/util
+
 LOCAL_LDLIBS += -lm -llog
 
 include $(BUILD_SHARED_LIBRARY)

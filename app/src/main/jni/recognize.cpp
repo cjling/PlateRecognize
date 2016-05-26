a1f2b9060619159a2808d1c6314165c179c79c55
cjling <cjling@aliyun.com>
2016-05-26 17:56:50 +0800

    修正输入字符串问题

--- app/src/main/jni/recognize.cpp
+++ app/src/main/jni/recognize.cpp
@@ -2,9 +2,10 @@
 // Created by cjling on 16/5/17.
 //
 
-#include <jni.h>
-#include <com_link_trust_platerecognize_PlateRecognizeLib.h>
 #include "include/easypr.h"
+#include <com_link_trust_platerecognize_PlateRecognizeLib.h>
+#include <jni.h>
+#include <string>
 
 JNIEXPORT jlong JNICALL Java_com_link_1trust_platerecognize_PlateRecognizeLib_InitSVM
   (JNIEnv * env, jobject obj, jstring svm_path)
@@ -23,11 +24,19 @@ JNIEXPORT jlong JNICALL Java_com_link_1trust_platerecognize_PlateRecognizeLib_In
 JNIEXPORT jstring JNICALL Java_com_link_1trust_platerecognize_PlateRecognizeLib_Recongize1
   (JNIEnv * env, jobject obj, jstring ann_path, jstring svm_path, jstring img_path)
   {
-        easypr::kDefaultSvmPath = "/sdcard/svm.xml";
-        easypr::kDefaultSvmPath = "/sdcard/svm.xml";
+        char ann_path_str[1024] = {0};
+        char svm_path_str[1024] = {0};
+        char img_path_str[1024] = {0};
+
+        strcpy(ann_path_str, env->GetStringUTFChars(ann_path, NULL));
+        strcpy(svm_path_str, env->GetStringUTFChars(svm_path, NULL));
+        strcpy(img_path_str, env->GetStringUTFChars(img_path, NULL));
+
+        easypr::kDefaultAnnPath = ann_path_str;
+        easypr::kDefaultSvmPath = svm_path_str;
 
         std::vector<std::string> results;
-        results = easypr::api::plate_recognize("/sdcard/tst.jpg", NULL, NULL);
+        results = easypr::api::plate_recognize(img_path_str, NULL, NULL);
 
         std::string all;
         for(int i = 0; i != results.size(); i++)
